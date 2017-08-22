314c6afe513f5b75adafe8c83cea40557b82a0ef
Satyam-sopho <Satyam-sopho@users.noreply.github.com>
2017-08-23 01:08:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 console.log('Loaded!');
 //move the image
-var img=document.getElementById('madi');
-img.onclick = fuction(){
-  madi.style.marginLeft='100px';  
+var img =document.getElementById('madi');
+img.onclick= fuction()
+{
+  img.style.marginLeft='100px';  
 };
