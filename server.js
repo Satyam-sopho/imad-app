300639afb045fac0add78a40a5e142b2a39fa803
Satyam-sopho <Satyam-sopho@users.noreply.github.com>
2017-08-23 22:36:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names= [];
-app.get('/submit-name/:name',function(req,res){ //URL:/submit-name?name=xxxxx
+app.get('/submit-name',function(req,res){ //URL:/submit-name?name=xxxxx
    //get the name from the request object
    //var name =req.params.name;
    var name =req.query.name;
